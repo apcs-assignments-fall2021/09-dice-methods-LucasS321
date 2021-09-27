94ca2812407a50eb5ba2e9c70f15fdf3bf652e3c
lucass <24lspeier@athenian.org>
2021-09-27 14:22:28 -0700

    sdaklfdsjf

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -3,7 +3,7 @@ public class MyMain {
     // Returns a random integer between 1 and 6
     public static int rollDie() {
         // REPLACE YOUR CODE HERE
-        return -1;
+        return ((int)(Math.random()*6))+1;
     }
 
     // Calculate the probability of rolling at least one 6 when rolling 
@@ -15,28 +15,160 @@ public class MyMain {
 
         // Your code should roll 6 dice 10,000 times, so you should have a
         // for loop such as:
+        int total = 0;
         for (int i = 0; i < 10000; i++) {
-
+            if (rollDie()  == 6) {
+                total += 1;
+                continue;
+            }
+            if (rollDie()  == 6) {
+                total += 1;
+                continue;
+            }
+            if (rollDie()  == 6) {
+                total += 1;
+                continue;
+            }
+            if (rollDie()  == 6) {
+                total += 1;
+                continue;
+            }
+            if (rollDie()  == 6) {
+                total += 1;
+                continue;
+            }
+            if (rollDie()  == 6) {
+                total += 1;
+                continue;
+            }
         }
 
-        return -1.0;
+        return total / 100;
     }
 
     // Calculate the probability of rolling at least two 6's when rolling 
     // twelve dice. Uses 10000 trials.
     public static double probabilityTwoSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int total = 0;
+        for (int i = 0; i < 10000; i++) {
+            int local = 0;
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (local >= 2) {
+                total += 1;
+            }
+        }
+
+        return total / 100;
     }
 
     // Calculate the probability of rolling at least three 6's when rolling
     // eighteen dice. Uses 10000 trials.
     public static double probabilityThreeSixes() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int total = 0;
+        for (int i = 0; i < 10000; i++) {
+            int local = 0;
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (rollDie()  == 6) {
+                local += 1;
+            }
+            if (local >= 3) {
+                total += 1;
+            }
+        }
+
+        return total / 100;
     }
 
     public static void main(String[] args) {
         // YOUR CODE HERE
+        System.out.println(rollDie()); //random number 1 - 6
+        System.out.println(probabilityOneSix()); // probability one 6
+        System.out.println(probabilityTwoSixes()); // probability two 6
+        System.out.println(probabilityThreeSixes()); // probability three 6
     }
 }
